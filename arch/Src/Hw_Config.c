232ea55ac0960051da5fa834bd26b97e176a0a73
duckhee <fain9301@yahoo.com>
2018-12-26 19:12:28 +0900

    setting

---
+++ arch/Src/Hw_Config.c
@@ -0,0 +1,551 @@
+/*
+ * (C) COPYRIGHT 2009 CRZ
+ *
+ * File Name : hw_config.c
+ * Author    : POOH
+ * Version   : V1.0
+ * Date      : 08/12/2009
+ */
+
+/* includes */
+/* Support files for GNU libc.  Files in the system namespace go here.
+   Files in the C namespace (ie those that do not start with an
+   underscore) go in .c.  */
+
+#include "Hw_Config.h"
+
+/* defines */
+
+#ifdef __GNUC__
+  #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+  //#define GETCHAR_PROTOTYPE int __io_getchar(void)
+#else
+  #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+ //#define GETCHAR_PROTOTYPE int fgetc(FILE *f)
+#endif 
+
+/* __GNUC__ */
+
+/* global variables */
+
+uint32_t ADC_ConvertedValueX = 0;
+uint32_t ADC_ConvertedValueX_1 = 0;
+static volatile uint32_t TimingDelay;
+
+/* functions */
+
+void delay_1_second(void)
+{
+    Delay(1000);
+}
+
+void delay_100_milli_second(void)
+{
+    Delay(100);
+}
+
+/*
+ * Name   : USART1_Init
+ * Input  : None
+ * Output : None
+ * Return : None
+ */
+void USART1_Init(void)
+{
+    USART_InitTypeDef USART_InitStructure;
+
+    /* USARTx configuration ------------------------------------------------------*/
+    /* USARTx configured as follow:
+     - BaudRate = 115200 baud  
+     - Word Length = 8 Bits
+     - One Stop Bit
+     - No parity
+     - Hardware flow control disabled (RTS and CTS signals)
+     - Receive and transmit enabled
+     */
+    USART_InitStructure.USART_BaudRate   = 115200;
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitStructure.USART_StopBits   = USART_StopBits_1;
+    USART_InitStructure.USART_Parity     = USART_Parity_No ;
+    USART_InitStructure.USART_HardwareFlowControl
+                                         = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode       = USART_Mode_Rx | USART_Mode_Tx;
+
+    /* Configure the USARTx */ 
+    USART_Init(USART1, &USART_InitStructure);
+
+    /* Enable the USART1 */
+    USART_Cmd(USART1, ENABLE);
+}
+
+/*
+ * Name   : RCC_Configuration
+ * Input  : None
+ * Output : None
+ * Return : None
+ */
+void RCC_Configuration(void)
+{
+    SystemInit();
+
+    /* Enable GPIOA clock */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+    /* Enable GPIOB clock */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+
+    /* Enable GPIOC clock */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+
+    /* Enable GPIOD clock */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+
+    /* Enable USART1 clocks */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+
+    /* Enable AFIO clock */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+
+    /* Enable DMA1 clock */
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+}
+
+/*******************************************************************************
+* Function Name  : NVIC_Configuration
+* Description    : Configures Vector Table base location.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void NVIC_Configuration(void)
+{ 
+    NVIC_InitTypeDef NVIC_InitStructure;
+
+#ifdef  VECT_TAB_RAM
+    /* Set the Vector Table base location at 0x20000000 */ 
+    NVIC_SetVectorTable(NVIC_VectTab_RAM, 0x0); 
+#else  /* VECT_TAB_FLASH  */
+    /* Set the Vector Table base location at 0x08000000 */ 
+    NVIC_SetVectorTable(NVIC_VectTab_FLASH, 0x0);   
+#endif
+
+    /* Configure one bit for preemption priority */
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
+
+    /* Enable the EXTI0 Interrupt */
+    NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+    /* Enable the EXTI1 Interrupt */
+    NVIC_InitStructure.NVIC_IRQChannel = EXTI1_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+}
+
+/*******************************************************************************
+* Function Name  : Set_USBClock
+* Description    : Configures USB Clock input (48MHz).
+* Input          : None.
+* Output         : None.
+* Return         : None.
+*******************************************************************************/
+void Set_USBClock(void)
+{
+    /* Select USBCLK source */
+    RCC_USBCLKConfig(RCC_USBCLKSource_PLLCLK_1Div5);
+
+    /* Enable USB clock */
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USB, ENABLE);
+}
+
+/*******************************************************************************
+* Function Name  : Enter_LowPowerMode.
+* Description    : Power-off system clocks and power while entering suspend mode.
+* Input          : None.
+* Output         : None.
+* Return         : None.
+*******************************************************************************/
+void Enter_LowPowerMode(void)
+{
+    /* Set the device state to suspend */
+    bDeviceState = SUSPENDED;
+
+#if 0
+    /* Clear EXTI Line18 pending bit */
+    EXTI_ClearITPendingBit(GPIO_EXTI_Line_KEY1);
+#endif
+
+    /* Request to enter STOP mode with regulator in low power mode */
+    PWR_EnterSTOPMode(PWR_Regulator_LowPower, PWR_STOPEntry_WFI);
+}
+
+/*******************************************************************************
+* Function Name  : Leave_LowPowerMode.
+* Description    : Restores system clocks and power while exiting suspend mode.
+* Input          : None.
+* Output         : None.
+* Return         : None.
+*******************************************************************************/
+void Leave_LowPowerMode(void)
+{
+    DEVICE_INFO *pInfo = &Device_Info;
+
+    /* Enable HSE */
+    RCC_HSEConfig(RCC_HSE_ON);
+
+    /* Wait till HSE is ready */
+    RCC_WaitForHSEStartUp();
+
+    /* Enable PLL */
+    RCC_PLLCmd(ENABLE);
+
+    /* Wait till PLL is ready */
+    while (RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET)
+    {}
+
+    /* Select PLL as system clock source */
+    RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+
+    /* Wait till PLL is used as system clock source */
+    while (RCC_GetSYSCLKSource() != 0x08)
+    {}
+
+    /* Set the device state to the correct state */
+    if (pInfo->Current_Configuration != 0)
+    {
+        /* Device configured */
+        bDeviceState = CONFIGURED;
+    }
+    else 
+    {
+        bDeviceState = ATTACHED;
+    }
+}
+
+/*******************************************************************************
+* Function Name  : USB_Interrupts_Config.
+* Description    : Configures the USB interrupts.
+* Input          : None.
+* Output         : None.
+* Return         : None.
+*******************************************************************************/
+void USB_Interrupts_Config(void)
+{
+    NVIC_InitTypeDef NVIC_InitStructure;
+
+    /* 2 bit for pre-emption priority, 2 bits for subpriority */
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+
+    /* Enable the USB interrupt */
+    NVIC_InitStructure.NVIC_IRQChannel = USB_LP_CAN1_RX0_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+    /* Enable the USB Wake-up interrupt */
+    NVIC_InitStructure.NVIC_IRQChannel = USBWakeUp_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+    
+    /* Enable the DMA1 Channel1 Interrupt */
+    NVIC_InitStructure.NVIC_IRQChannel = DMA1_Channel1_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+}
+
+/*******************************************************************************
+* Function Name  : USB_Cable_Config.
+* Description    : Software Connection/Disconnection of USB Cable.
+* Input          : NewState: new state.
+* Output         : None.
+* Return         : None
+*******************************************************************************/
+void USB_Cable_Config (FunctionalState NewState)
+{ 
+    if (NewState != DISABLE)
+    {
+        GPIO_ResetBits(GPIO_USB_DISCONNECT, GPIO_USB_DISCONNECT_PIN);
+    }
+    else
+    {
+        GPIO_SetBits(GPIO_USB_DISCONNECT, GPIO_USB_DISCONNECT_PIN);
+    }
+}
+
+/*
+ * Name   : GPIO_Configuration
+ * Input  : None
+ * Output : None
+ * Return : None
+ */
+void GPIO_Configuration(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+
+    // UART configuration ...
+
+    /* Configure USARTx_Tx as alternate function push-pull */
+    GPIO_InitStructure.GPIO_Pin   = GPIO_USART_Tx_Pin;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AF_PP;
+    GPIO_Init(GPIO_USART, &GPIO_InitStructure);
+
+    /* Configure USARTx_Rx as input floating */
+    GPIO_InitStructure.GPIO_Pin  = GPIO_USART_Rx_Pin;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_Init(GPIO_USART, &GPIO_InitStructure);
+
+    // Key configuration ...
+
+    /* Configure gpio as input : Button WKUP & USER */
+    GPIO_InitStructure.GPIO_Pin  = GPIO_KEY1_PIN | GPIO_KEY2_PIN;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_Init(GPIO_KEY, &GPIO_InitStructure);
+
+    // LED configuration ...
+
+    /* Configure gpio as output : LED1, LED2, LED3 */
+    GPIO_InitStructure.GPIO_Pin   = GPIO_LED1_PIN | GPIO_LED2_PIN | GPIO_LED3_PIN ;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_PP;
+    GPIO_Init(GPIO_LED, &GPIO_InitStructure);
+
+    // USB_DISCONNECT configuration ...
+
+    /* USB_DISCONNECT used as USB pull-up */
+    GPIO_InitStructure.GPIO_Pin   = GPIO_USB_DISCONNECT_PIN;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_OD;
+    GPIO_Init(GPIO_USB_DISCONNECT, &GPIO_InitStructure);
+
+#ifdef  BOARD_DEF_MANGO_Z1
+    // CC2520 RF GPIO configuration ...
+
+    GPIO_InitStructure.GPIO_Pin
+        = GPIO_RF_GPIO0_PIN | GPIO_RF_GPIO1_PIN
+        | GPIO_RF_GPIO2_PIN | GPIO_RF_GPIO3_PIN | GPIO_RF_GPIO4_PIN;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_IN_FLOATING;
+    GPIO_Init(GPIO_RF_GPIO0_4, &GPIO_InitStructure);
+
+    GPIO_InitStructure.GPIO_Pin   = GPIO_RF_GPIO5_PIN;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_IN_FLOATING;
+    GPIO_Init(GPIO_RF_GPIO5, &GPIO_InitStructure);
+#endif
+}
+
+/*******************************************************************************
+* Function Name : EXTI_Configuration.
+* Description   : Configure the EXTI lines for Key and Tamper push buttons.
+* Input         : None.
+* Output        : None.
+* Return value  : The direction value.
+*******************************************************************************/
+void EXTI_Configuration(void)
+{ 
+    EXTI_InitTypeDef EXTI_InitStructure;
+
+    /* Configure gpio as input : Button Left-WKUP */
+    /* Connect EXTI Line to gpio pin */
+    GPIO_EXTILineConfig(GPIO_PORTSOURCE_KEY, GPIO_PINSOURCE_KEY1);
+
+    /* Configure EXTI Line to generate an interrupt */
+    EXTI_InitStructure.EXTI_Line    = GPIO_EXTI_Line_KEY1;
+    EXTI_InitStructure.EXTI_Mode    = EXTI_Mode_Interrupt;
+    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
+    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+    EXTI_Init(&EXTI_InitStructure);
+
+    /* Configure gpio as input : Button Right-USER */
+    /* Connect EXTI Line to gpio pin */
+    GPIO_EXTILineConfig(GPIO_PORTSOURCE_KEY, GPIO_PINSOURCE_KEY2);
+
+    /* Configure EXTI Line to generate an interrupt */  
+    EXTI_InitStructure.EXTI_Line    = GPIO_EXTI_Line_KEY2;
+    EXTI_InitStructure.EXTI_Mode    = EXTI_Mode_Interrupt;
+    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
+    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+    EXTI_Init(&EXTI_InitStructure);
+
+    /* USB Wakeup from suspend event */
+    EXTI_ClearITPendingBit(EXTI_Line18);
+    EXTI_InitStructure.EXTI_Line    = EXTI_Line18; // USB resume from suspend mode
+    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
+    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+    EXTI_Init(&EXTI_InitStructure);
+}
+
+/*******************************************************************************
+* Function Name  : Get_SerialNum.
+* Description    : Create the serial number string descriptor.
+* Input          : None.
+* Output         : None.
+* Return         : None.
+*******************************************************************************/
+void Get_SerialNum(void)
+{
+  uint32_t Device_Serial0, Device_Serial1, Device_Serial2;
+  
+  Device_Serial0 = *(uint32_t*)(0x1FFFF7E8);
+  Device_Serial1 = *(uint32_t*)(0x1FFFF7EC);
+  Device_Serial2 = *(uint32_t*)(0x1FFFF7F0);
+  
+  if(Device_Serial0 != 0)
+  {
+     CustomHID_StringSerial[2] = (uint8_t)(Device_Serial0 & 0x000000FF);  
+     CustomHID_StringSerial[4] = (uint8_t)((Device_Serial0 & 0x0000FF00) >> 8);
+     CustomHID_StringSerial[6] = (uint8_t)((Device_Serial0 & 0x00FF0000) >> 16);
+     CustomHID_StringSerial[8] = (uint8_t)((Device_Serial0 & 0xFF000000) >> 24);  
+
+     CustomHID_StringSerial[10] = (uint8_t)(Device_Serial1 & 0x000000FF);  
+     CustomHID_StringSerial[12] = (uint8_t)((Device_Serial1 & 0x0000FF00) >> 8);
+     CustomHID_StringSerial[14] = (uint8_t)((Device_Serial1 & 0x00FF0000) >> 16);
+     CustomHID_StringSerial[16] = (uint8_t)((Device_Serial1 & 0xFF000000) >> 24); 
+
+     CustomHID_StringSerial[18] = (uint8_t)(Device_Serial2 & 0x000000FF);  
+     CustomHID_StringSerial[20] = (uint8_t)((Device_Serial2 & 0x0000FF00) >> 8);
+     CustomHID_StringSerial[22] = (uint8_t)((Device_Serial2 & 0x00FF0000) >> 16);
+     CustomHID_StringSerial[24] = (uint8_t)((Device_Serial2 & 0xFF000000) >> 24); 
+  }   
+}
+
+/**
+  * @brief  Inserts a delay time.
+  * @param  nTime: specifies the delay time length, in milliseconds.
+  * @retval None
+  */
+void Delay(__IO uint32_t nTime)
+{ 
+  TimingDelay = nTime;
+
+  while(TimingDelay != 0);
+}
+
+/**
+  * @brief  Decrements the TimingDelay variable.
+  * @param  None
+  * @retval None
+  */
+void TimingDelay_Decrement(void)
+{
+  if (TimingDelay != 0x00)
+  { 
+    TimingDelay--;
+  }
+}
+
+#ifdef  BOARD_DEF_MANGO_Z1
+/*******************************************************************************
+* Function Name  : SPIx_Init
+* Description    : Configures the different SDIO.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void SPIx_Init(void)
+{
+    DEBUG_MSG_FUNC_START;
+
+    GPIO_InitTypeDef GPIO_InitStructure;
+    SPI_InitTypeDef  SPI_InitStructure;
+
+    /* GPIOA, GPIOB and SPI1 clock enable */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_SPI1, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+
+    /* Configure SPI1 pins: SCK, MISO and MOSI ---------------------------------*/
+    GPIO_InitStructure.GPIO_Pin
+        = GPIO_RF_SPI_CLK_PIN | GPIO_RF_SPI_MISO_PIN | GPIO_RF_SPI_MOSI_PIN;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_Init(GPIO_RF_SPI, &GPIO_InitStructure);
+
+    /* Configure SPI1 pins: nCS ------------------------------------------------*/
+    GPIO_InitStructure.GPIO_Pin = GPIO_RF_SPI_NCS_PIN;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_Init(GPIO_RF_SPI, &GPIO_InitStructure);
+
+    /* Configure SPI1 pins: VREG_EN and RESETn ---------------------------------*/
+    GPIO_InitStructure.GPIO_Pin = GPIO_RF_RESET_PIN | GPIO_RF_VREG_EN_PIN;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_Init(GPIO_RF_RESET, &GPIO_InitStructure);
+
+    /* SPI1 Config -------------------------------------------------------------*/
+    SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
+    SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
+    SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
+
+    SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
+    SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
+
+    SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
+    SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_16;   // 72MHz / 16 = 4.5MHz
+    SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
+    SPI_InitStructure.SPI_CRCPolynomial = 7;
+    SPI_Init(SPI1, &SPI_InitStructure);
+
+    /* Enable SPI1 */
+    SPI_Cmd(SPI1, ENABLE);
+
+    GPIO_SetBits(GPIO_RF_RESET, GPIO_RF_RESET_PIN);
+    GPIO_SetBits(GPIO_RF_VREG_EN, GPIO_RF_VREG_EN_PIN);
+}
+#endif /* BOARD_DEF_MANGO_Z1 */
+
+
+uint8_t USART_GetCharacter(USART_TypeDef *  usart_p)
+{
+    uint8_t data;
+
+    /* Loop until the end of transmission */
+    while(USART_GetFlagStatus(usart_p, USART_FLAG_RXNE) == RESET);
+
+    /* Write a character to the USART */
+    data = USART_ReceiveData(usart_p);
+
+    USART_SendData(usart_p, data);
+    while(USART_GetFlagStatus(usart_p, USART_FLAG_TXE) == RESET);
+
+    if( data == '\r' )  return (int)('\n');
+    else                return(data);
+}
+
+//need to set int _write(int file, char* ch, int len)
+
+/*
+ * Name   : putchar
+ * Input  : None
+ * Output : None
+ * Return : None
+ */
+PUTCHAR_PROTOTYPE
+{
+    /* Write a character to the USART */  
+    if( ch == '\n') {
+        USART_SendData(USART1, '\r');
+        while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+        USART_SendData(USART1, '\n');
+    }else {
+        USART_SendData(USART1, (u8) ch);
+    }
+   USART_SendData(USART1, (uint8_t)ch);
+
+    /* Loop until the end of transmission */
+    while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+
+    return ch;
+}
+
